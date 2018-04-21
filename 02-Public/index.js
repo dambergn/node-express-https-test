8dc54f09106213bddae7f48c71216671f58e1d17
dambergn <dambergn@gmail.com>
2018-04-21 03:09:00 -0700

    created ssl with public page

---
+++ 02-Public/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
+
+const port = 8000;
+
+const options = {
+    key: fs.readFileSync('./ssl/ssl-key.key'),
+    cert: fs.readFileSync('./ssl/ssl-crt.crt'),
+};
+
+const app = express();
+
+const server = https.createServer(options, app).listen(port, function(){
+  console.log("Express server listening on port " + port);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendfile(index.html);
+});
